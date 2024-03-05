911e3b9eb392908a6e967f8f95d75742ae3de349
Aabishkar Shrestha <Aabishkar@adsaro.com>
2024-03-05 15:32:40 +0545

    Push

--- component/header.tsx
+++ component/header.tsx
@@ -1,38 +1,40 @@
 import { useState, useEffect } from 'react';
 
 export default function Header() {
-  
-    useEffect(() => {
-        const words = ["Hello, World!", "I am Aabishkar Shrestha!", "I am Web Developer."];
-        let i = 0;
-        let j = 0;
-        let currentWord = "";
-        let isDeleting = false;
+  <>
+  aabishkar
+  </>
+    // useEffect(() => {
+    //     const words = ["Hello, World!", "I am Aabishkar Shrestha!", "I am Web Developer."];
+    //     let i = 0;
+    //     let j = 0;
+    //     let currentWord = "";
+    //     let isDeleting = false;
     
-        function type() {
-          currentWord = words[i];
-          if (isDeleting) {
-            document.getElementById("typewriter").textContent = currentWord.substring(0, j - 1);
-            j--;
-            if (j === 0) {
-              isDeleting = false;
-              i++;
-              if (i === words.length) {
-                i = 0;
-              }
-            }
-          } else {
-            document.getElementById("typewriter").textContent = currentWord.substring(0, j + 1);
-            j++;
-            if (j === currentWord.length) {
-              isDeleting = true;
-            }
-          }
-          setTimeout(type, 100);
-        }
+    //     function type() {
+    //       currentWord = words[i];
+    //       if (isDeleting) {
+    //         document.getElementById("typewriter").textContent = currentWord.substring(0, j - 1);
+    //         j--;
+    //         if (j === 0) {
+    //           isDeleting = false;
+    //           i++;
+    //           if (i === words.length) {
+    //             i = 0;
+    //           }
+    //         }
+    //       } else {
+    //         document.getElementById("typewriter").textContent = currentWord.substring(0, j + 1);
+    //         j++;
+    //         if (j === currentWord.length) {
+    //           isDeleting = true;
+    //         }
+    //       }
+    //       setTimeout(type, 100);
+    //     }
     
-        type();
-      }, []);
+    //     type();
+    //   }, []);
   
     return (
     <div>
